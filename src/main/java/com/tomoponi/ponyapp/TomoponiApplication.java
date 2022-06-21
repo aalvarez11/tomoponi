99759278741a88f5605daa3b8b0aeba5d51f0e0b
aalvarez11 <aalvarez1@email.arizona.edu>
2022-06-21 13:51:11 -0700

    Initial commit

---
+++ src/main/java/com/tomoponi/ponyapp/TomoponiApplication.java
@@ -0,0 +1,13 @@
+package com.tomoponi.ponyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomoponiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomoponiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomoponi/ponyapp/TomoponiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomoponi.ponyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomoponiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
