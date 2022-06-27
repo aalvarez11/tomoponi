dc1567e6c624b5e9978d9ac4dc8d94255706f01b
aalvarez11 <aalvarez1@email.arizona.edu>
2022-06-27 09:17:01 -0700

    user now tracks coin count

--- src/main/java/com/tomoponi/ponyapp/model/User.java
+++ src/main/java/com/tomoponi/ponyapp/model/User.java
@@ -5,11 +5,13 @@ import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
 import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Setter
 @Getter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
@@ -25,13 +27,30 @@ public class User {
     String email;
     @NonNull
     String password;
+    @NonNull
+    int coins;
 
     // each user has a personal bag/inventory only they can see and access
     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "bag_id")
+    @ToString.Exclude
     private Bag bag;
 
     // each user can have multiple pets to care for
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
     Set<Pet> pets = new LinkedHashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && coins == user.coins && username.equals(user.username) && email.equals(user.email) && password.equals(user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, email, password);
+    }
 }
