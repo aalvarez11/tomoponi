a210ccc520a86f4a918b1a3af6ae83aeacdc2ad3
aalvarez11 <aalvarez1@email.arizona.edu>
2022-06-28 15:34:50 -0700

    security config made, any visitor can now see the index and the login page

---
+++ src/main/java/com/tomoponi/ponyapp/security/AppSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.tomoponi.ponyapp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/index").permitAll();
+    }
+}
