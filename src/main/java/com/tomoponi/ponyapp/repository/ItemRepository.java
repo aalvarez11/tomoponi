1318dfcc9003c860dbc5aa9f8844588f27f68fd0
aalvarez11 <aalvarez1@email.arizona.edu>
2022-06-29 14:24:35 -0700

    made item repository a repository, added findbyname abstract method

--- src/main/java/com/tomoponi/ponyapp/repository/ItemRepository.java
+++ src/main/java/com/tomoponi/ponyapp/repository/ItemRepository.java
@@ -1,10 +1,12 @@
 package com.tomoponi.ponyapp.repository;
 
 import com.tomoponi.ponyapp.model.Item;
-import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-@NoRepositoryBean
+import java.util.Optional;
+
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
-
+    Optional<Item> findByNameIgnoreCase(String name);
 }
