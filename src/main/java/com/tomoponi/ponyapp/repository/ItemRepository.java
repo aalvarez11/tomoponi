c273c3711c531d0f2ef2a87377246b5e3b2cb38b
aalvarez11 <aalvarez1@email.arizona.edu>
2022-06-29 13:46:14 -0700

    finditembyname abstract method

--- src/main/java/com/tomoponi/ponyapp/repository/ItemRepository.java
+++ src/main/java/com/tomoponi/ponyapp/repository/ItemRepository.java
@@ -4,7 +4,9 @@ import com.tomoponi.ponyapp.model.Item;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 @NoRepositoryBean
 public interface ItemRepository extends JpaRepository<Item, Integer> {
-
+    Optional<Item> findByName(String name);
 }
