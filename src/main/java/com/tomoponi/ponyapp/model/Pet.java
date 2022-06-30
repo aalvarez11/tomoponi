ea100fc7ad904b39ce1072caad0f268c58b7575f
aalvarez11 <aalvarez1@email.arizona.edu>
2022-06-30 10:26:16 -0700

    changed some fields, filled in annotations, added constructor

--- src/main/java/com/tomoponi/ponyapp/model/Pet.java
+++ src/main/java/com/tomoponi/ponyapp/model/Pet.java
@@ -1,28 +1,63 @@
 package com.tomoponi.ponyapp.model;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Pet {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @NonNull
+    String name;
+    @NonNull
     String image;
-    int maxHP;
-    int currHP;
+    @NonNull
     int level;
+    @NonNull
+    int health;
+    @NonNull
     int hunger;
+    @NonNull
     int happiness;
+    @Enumerated(value = EnumType.STRING)
     ElementType element;
 
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
     @JoinColumn(name = "user_id")
+    @ToString.Exclude
     User user;
 
-    public User getUser() {
-        return user;
+    public Pet(String name, String image, int level, int health, int hunger, int happiness, ElementType element) {
+        this.name = name;
+        this.image = image;
+        this.level = level;
+        this.health = health;
+        this.hunger = hunger;
+        this.happiness = happiness;
+        this.element = element;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pet pet = (Pet) o;
+        return health == pet.health && level == pet.level && hunger == pet.hunger && happiness == pet.happiness && name.equals(pet.name) && image.equals(pet.image) && element == pet.element;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, image, health, level, hunger, happiness, element);
     }
 }
