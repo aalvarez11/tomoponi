423a8e323cf6d502cdf8be064ce099b36d70688b
aalvarez11 <aalvarez1@email.arizona.edu>
2022-06-30 16:13:18 -0700

    mapping for registering a new account

--- src/main/java/com/tomoponi/ponyapp/controller/MainController.java
+++ src/main/java/com/tomoponi/ponyapp/controller/MainController.java
@@ -1,8 +1,12 @@
 package com.tomoponi.ponyapp.controller;
 
+import com.tomoponi.ponyapp.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
@@ -11,6 +15,18 @@ public class MainController {
     @GetMapping("/")
     public String showHome() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showForm(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String submitForm(@ModelAttribute("user") User user) {
+        return "register_success";
     }
 
     @GetMapping("/login")
