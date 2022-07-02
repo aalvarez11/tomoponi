9fc2053093bfd429a4fca09d7064021d6a4a4f92
aalvarez11 <aalvarez1@email.arizona.edu>
2022-07-02 14:33:19 -0700

    find user by email w/ query

--- src/main/java/com/tomoponi/ponyapp/repository/UserRepository.java
+++ src/main/java/com/tomoponi/ponyapp/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.tomoponi.ponyapp.repository;
 
 import com.tomoponi.ponyapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    Optional<User> findByEmail(String email);
 }
