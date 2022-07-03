37c3a344c325bdd345900bd0cb6628c59364ad50
aalvarez11 <aalvarez1@email.arizona.edu>
2022-07-02 19:26:39 -0700

    added encryption and authorization

--- src/main/java/com/tomoponi/ponyapp/security/AppSecurityConfiguration.java
+++ src/main/java/com/tomoponi/ponyapp/security/AppSecurityConfiguration.java
@@ -1,23 +1,71 @@
 package com.tomoponi.ponyapp.security;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    AppUserDetailsService appUserDetailsService;
+
+    @Autowired
+    public AppSecurityConfiguration(AppUserDetailsService appUserDetailsService) {
+        this.appUserDetailsService = appUserDetailsService;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        // the default strength is 10, so leaving the constructor empty
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(appUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**");
+        web.ignoring().antMatchers("/resources/**", "/static/**",
+                                    "/css/**", "/js/**", "/img/**",
+                                    "/assets/**", "/pets/**", "/types/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/index", "/login", "/register").permitAll();
+                .antMatchers("/", "/index", "/login", "/register").permitAll()
+                .and()
+                .formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password")
+                .loginProcessingUrl("/login/authenticate").defaultSuccessUrl("/")
+                .failureUrl("/login?error=true").permitAll()
+                .and()
+                .logout().invalidateHttpSession(true).clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/").permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/403");
     }
 }
