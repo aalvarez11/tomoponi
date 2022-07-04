0f3b8369f847ebd7ad1d9d2a2a67fd806517f42d
aalvarez11 <aalvarez1@email.arizona.edu>
2022-07-04 12:12:45 -0700

    pet controller created

---
+++ src/main/java/com/tomoponi/ponyapp/controller/PetController.java
@@ -0,0 +1,26 @@
+package com.tomoponi.ponyapp.controller;
+
+import com.tomoponi.ponyapp.services.PetService;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping(value = "pets")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PetController {
+    final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    @GetMapping("/viewpets")
+    public String showPets() {
+        return "pets";
+    }
+}
